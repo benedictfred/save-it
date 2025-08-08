cedc90e458b51a51b8710fcb9e3dfc32e7ce5d87
Ugochukwu Nebolisa <nebolisaugochukwu@gmail.com>
2025-08-09 00:35:05 +0100

    chore: hosted the postgres db on prisma

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import bcrypt from "bcrypt";
 
 export const prisma = new PrismaClient({
@@ -16,23 +17,25 @@ export const prisma = new PrismaClient({
       userId: true,
     },
   },
-}).$extends({
-  query: {
-    user: {
-      // @ts-ignore
-      async create({ model, operation, args, query }) {
-        const hashedPassword = await bcrypt.hash(args.data.password, 12);
-        args.data = {
-          ...args.data,
-          password: hashedPassword,
-          accountNumber:
-            args.data.phoneNumber.length > 10
-              ? args.data.phoneNumber.slice(1)
-              : args.data.phoneNumber,
-        };
+})
+  .$extends({
+    query: {
+      user: {
+        // @ts-ignore
+        async create({ model, operation, args, query }) {
+          const hashedPassword = await bcrypt.hash(args.data.password, 12);
+          args.data = {
+            ...args.data,
+            password: hashedPassword,
+            accountNumber:
+              args.data.phoneNumber.length > 10
+                ? args.data.phoneNumber.slice(1)
+                : args.data.phoneNumber,
+          };
 
-        return query(args);
+          return query(args);
+        },
       },
     },
-  },
-});
+  })
+  .$extends(withAccelerate());
