c36b68c6496fb512509f26adc5b2ae733b44790c
Ugochukwu Nebolisa <nebolisaugochukwu@gmail.com>
2025-08-09 19:03:57 +0100

    chore: changed the backend structure

--- backend/utils/sendToken.ts
+++ backend/utils/sendToken.ts
@@ -5,6 +5,5 @@ export default function sendToken(res: Response, token: string) {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    partitioned: true,
   });
 }
