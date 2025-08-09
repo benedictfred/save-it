5ce449f0b637360dc99c35cf54825b7a08089cd2
Ugochukwu Nebolisa <nebolisaugochukwu@gmail.com>
2025-08-09 01:04:07 +0100

    fix: adjusted sameSite constraint to allow communication between the client and the server

--- backend/src/utils/sendToken.ts
+++ backend/src/utils/sendToken.ts
@@ -4,7 +4,7 @@ export default function sendToken(res: Response, token: string) {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 }
