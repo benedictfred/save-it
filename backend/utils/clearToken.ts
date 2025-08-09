62cc9986c746f4f9c411d8ebaa0bcfe6cbe3f39d
Ugochukwu Nebolisa <nebolisaugochukwu@gmail.com>
2025-08-09 19:36:08 +0100

    chore: added the path attribute to send token function

--- backend/utils/clearToken.ts
+++ backend/utils/clearToken.ts
@@ -4,6 +4,7 @@ export default function clearToken(res: Response) {
   res.clearCookie("jwt", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
+    path: "/",
     sameSite: "none",
     partitioned: true,
   });
