f3cd7ce817e73adae783bb82bdcd21313064189b
Ugochukwu Nebolisa <nebolisaugochukwu@gmail.com>
2025-10-08 14:19:20 +0100

    feat: added a formatNameToCapitalize helper function

--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -37,3 +37,15 @@ export const formatCountdown = (secs: number) => {
   const seconds = (secs % 60).toString().padStart(2, "0");
   return `${minutes}:${seconds}`;
 };
+
+export const formatNameToCapitalize = (name: string) => {
+  const names = name?.trim().split(" ");
+
+  if (names?.length > 1) {
+    return names
+      .map((n) => n.charAt(0).toUpperCase() + n.slice(1).toLowerCase())
+      .join(" ");
+  }
+
+  return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+};
